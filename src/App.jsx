0a338cb0c8f3feb7695e9c3e4ab42fddca85c205
coffmanjrp <coffmanjrp@gmail.com>
2021-08-28 05:29:43 -0700

    Add products data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import image1 from './image/image1.png';
-import image2 from './image/image2.png';
-import image3 from './image/image3.png';
+import products from './utils/data';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <div className="slide">
-        <h2>Airpods</h2>
-        <h4>$199</h4>
-        <img src={image1} alt="Airpods" />
-        <a href="#!" className="btn">
-          Buy Now
-        </a>
-      </div>
-      <div className="slide">
-        <h2>iPhone 12</h2>
-        <h4>$799</h4>
-        <img src={image2} alt="iPhone 12" />
-        <a href="#!" className="btn">
-          Buy Now
-        </a>
-      </div>
-      <div className="slide">
-        <h2>iPad</h2>
-        <h4>$599</h4>
-        <img src={image3} alt="iPad" />
-        <a href="#!" className="btn">
-          Buy Now
-        </a>
-      </div>
+      {products.length > 0 &&
+        products.map((product) => (
+          <div key={product.id} className="slide">
+            <h2>{product.name}</h2>
+            <h4>${product.price}</h4>
+            <img src={product.image} alt={product.alt} />
+            <a href={product.link} className="btn">
+              Buy Now
+            </a>
+          </div>
+        ))}
     </div>
   );
 }
---
+++ src/utils/data.js
@@ -0,0 +1,32 @@
+import image1 from '../image/image1.png';
+import image2 from '../image/image2.png';
+import image3 from '../image/image3.png';
+
+const products = [
+  {
+    id: 1,
+    name: 'Airpods',
+    price: 199,
+    image: image1,
+    alt: 'Airpods',
+    link: '#!',
+  },
+  {
+    id: 2,
+    name: 'iPhone 12',
+    price: 799,
+    image: image2,
+    alt: 'iPhone 12',
+    link: '#!',
+  },
+  {
+    id: 3,
+    name: 'iPad',
+    price: 599,
+    image: image3,
+    alt: 'iPad',
+    link: '#!',
+  },
+];
+
+export default products;
